0817342b38aa5865eabc04035d13d1d37add4a0c
CarlosIzarra09 <eduizarra2@hotmail.com>
2023-09-08 00:35:17 -0500

    feat: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({ });
+  
+  app.useGlobalPipes(new ValidationPipe({
+    disableErrorMessages: false,
+    transform: true
+  }));
+
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('Documentación de API del Proyecto Dog Microservice.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(+process.env.PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap();
